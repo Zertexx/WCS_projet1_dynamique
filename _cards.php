0d3f274b82c3a35105adf1c9b546d9bfd22682b9
Maxime Vasseur <zertouflex@gmail.com>
2019-03-15 11:03:19 +0100

    tableau complet pour trombinoscope

--- _cards.php
+++ _cards.php
@@ -1,18 +1,12 @@
 <?php
 $cards = [
 
-    'guillaume',
-    'maxime',
-    'jimmy',
-    'florent',
-    'guillaume',
-    'maxime',
-    'jimmy',
-    'florent',
-    'guillaume',
-    'maxime',
-    'jimmy',
-    'florent',
+        ['prenom' => 'guillaume', 'nom' => 'Loup', 'langage' => 'Langage JS'],
+        ['prenom' => 'maxime', 'nom' => 'Vasseur', 'langage' => 'Langage PHP'],
+        ['prenom' => 'florent', 'nom' => 'Loup', 'langage' => 'Langage JS'],
+        ['prenom' => 'jimmy', 'nom' => 'Loup', 'langage' => 'Langage JS'],
+        ['prenom' => 'Guillaume', 'nom' => 'Loup', 'langage' => 'Langage JS'],
+        ['prenom' => 'Guillaume', 'nom' => 'Loup', 'langage' => 'Langage JS'],
 ];
 
 
@@ -25,13 +19,13 @@ $cards = [
     </div>
     <div class="row">
         <?php
-        foreach ($cards as $profile){
+        foreach ($cards as $students){
             ?>
         <div class="trombi-box-pict col-12 col-md-4">
-            <a href="profil-<?php echo ($profile) ?>.html"><img class="trombi-pict" src="images/<?php echo ($profile) ?>.jpg" alt="photo de profil 1"></a>
+            <a href="profil-<?php echo $students['prenom'] ?>.php"><img class="trombi-pict" src="images/<?php echo $students['prenom'] ?>.jpg" alt="photo de profil 1"></a>
             <div class="trombi-bloc-text col-12">
-                <p class="trombi-text-title">Guillaume Loup</p>
-                <p class="trombi-text-subtitle">Langage JS</p>
+                <p class="trombi-text-title"><?php echo $students['prenom'] . ' ' . $students['nom'];?></p>
+                <p class="trombi-text-subtitle"><?php echo $students['langage'];?></p>
             </div>
 
         </div>
